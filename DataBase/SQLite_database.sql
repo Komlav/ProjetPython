0d5fa0eeae2f5f2f14c5d9cfb7f130d9a8e8d53b
Komlav <yurimaotchiha@gmail.com>
2023-05-31 19:46:57 +0000

    Mise à jour

--- DataBase/SQLite_database.sql
+++
@@ -1,2 +0,0 @@
--- SQLite
-ALTER TABLE responsableAdmin DROP COLUMN classes;
--- DataBase/Tables des chargés.sql
+++
@@ -1,3 +0,0 @@
--- SQLite3
-UPDATE Chargé SET Classes = "[]" WHERE  Nom = "Bernie";
-UPDATE Chargé SET Classes = "[]" WHERE  Nom = "sarr";
